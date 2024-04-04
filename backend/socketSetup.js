6a867616c89e657a148aee748f24f401eaaae649
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2024-04-04 18:43:53 +0530

    Added more functionality

--- backend/socketSetup.js
+++ backend/socketSetup.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 
 function createConnection(server) {
+  const users = {};
   const io = new Server(server, {
     cors: { origin: "*" },
   });
@@ -8,10 +9,19 @@ function createConnection(server) {
   io.on("connection", (socket) => {
     console.log(`user connected`, socket.id);
 
-    socket.on('editor-update', (data) => {
+    socket.on("user-join", (userName) => {
+      users[socket.id] = userName;
+      io.emit("joined-user", { userName: userName, userId: socket.id });
+    });
 
-      socket.broadcast.emit('broadcast-update', data)
-    })
+    socket.on("editor-update", (data) => {
+      socket.broadcast.emit("broadcast-update", data);
+    });
+
+    socket.on("disconnect", () => {
+      io.emit("user-disconnect", socket.id);
+      console.log("User disconnected", users[socket.id]);
+    });
   });
 
   return io;
--- frontend/js/monaco-editor.js
+++ frontend/js/monaco-editor.js
@@ -1,48 +1,99 @@
-window.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const userSection = document.querySelector(".user-section");
+  const userName = prompt(`Give your name to use editor`);
+
   const HOST = `http://localhost:8001`;
   const socket = io(HOST);
 
-  let editorContent = "";
+  let editorContent = null;
   let editorPosition = { lineNumber: 0, column: 0 };
 
-  function configEditor() {
+  const editor = initializeEditor();
+
+  function initializeEditor() {
     require.config({
       paths: {
         vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs",
       },
     });
 
+    let editorInstance = null;
     require(["vs/editor/editor.main"], function () {
-      const editor = monaco.editor.create(
+      editorInstance = monaco.editor.create(
         document.getElementById("monaco-editor"),
         {
           language: "javascript",
           automaticLayout: true,
-          value: editorContent,
+          value: editorInstance,
         }
       );
+      handleSocketEvents(editorInstance);
+    });
+    return editorInstance;
+  }
 
-      editor.onKeyDown(
-        () => {
-          editorContent = editor.getModel().getValue();
-          editorPosition = editor.getPosition();
+  function handleSocketEvents(editorInstance) {
+    editorInstance.onKeyDown(() => {
+      editorContent = editorInstance.getModel().getValue().trim();
+      editorPosition = editorInstance.getPosition();
 
-          socket.emit("editor-update", {
-            content: editorContent,
-            position: editorPosition,
-          });
-        },
-        null,
-        null,
-        true
-      );
-
-      socket.on("broadcast-update", (data) => {
-        editor.getModel().setValue(data.content);
-        editor.setPosition(data.position);
+      socket.emit("editor-update", {
+        content: editorContent,
+        position: editorPosition,
       });
+    });
+
+    socket.on("broadcast-update", (data) => {
+      editorInstance.getModel().setValue(data.content);
+      editorInstance.setPosition(data.position);
+    });
+
+    if (userName) {
+      socket.emit(`user-join`, userName);
+    }
+
+    socket.on("joined-user", (userDetails) => {
+      showUserJoinNotification(userDetails, 1500);
+    });
+
+    // Handle user disconnect notification
+    socket.on("user-disconnect", (userId) => {
+      const disconnectedUser = document.querySelector(`[data-id="${userId}"]`);
+      if (disconnectedUser) {
+        userSection.removeChild(disconnectedUser);
+        showDisconnectNotification(userId);
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
     });
   }
 
-  configEditor();
+  function showUserJoinNotification(userDetails, timer = 1500) {
+    const popUp = document.createElement("div");
+    if (userDetails.userId === socket.id) {
+      popUp.textContent = `You Joined to use Editor`;
+    } else {
+      popUp.textContent = `${userDetails.userName}, Collab with your code.`;
+    }
+    popUp.classList.add("popup");
+    popUp.dataset.id = userDetails.userId;
+    userSection.appendChild(popUp);
+
+    setTimeout(() => {
+      popUp.remove();
+    }, timer);
+  }
+
+  function showDisconnectNotification(userId) {
+    const popUp = document.createElement("div");
+    popUp.textContent = `User ${userId} disconnected`;
+    popUp.classList.add("popup");
+    userSection.appendChild(popUp);
+
+    setTimeout(() => {
+      popUp.remove();
+    }, 1500);
+  }
 });
